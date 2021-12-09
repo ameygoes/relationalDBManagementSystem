28c61c655eea30c8d98388c25ab9f142a813e29b
Amey <bamey2241997@gmail.com>
2021-12-09 23:22:28 +0530

    added changes

--- CICD/runServer.sh
+++ CICD/runServer.sh
@@ -5,7 +5,7 @@ sudo mkdir -p $DESTINATION
 
 sudo cp -rvf $WORKSPACE/* $DESTINATION
 sudo chown -R $USER:$USER /usr/lib/python3.7/site-packages
-echo "/var/opt/rdbms/relationalDBManagementSystem" > /usr/lib/python3.7/addProjectPath.pth
+echo "/var/opt/rdbms/relationalDBManagementSystem" > /usr/lib/python3.7/site-packages/addProjectPath.pth
 
 cd $DESTINATION/rdbms/
 echo "Starting Server..."

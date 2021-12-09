d4dbfe6ce97f9a7fb95a7fb11d90c1ad4110ac10
Amey <bamey2241997@gmail.com>
2021-12-09 16:27:13 +0530

    resolved conflicts

--- rdbms/relationalDataBaseManagement/settings.py
+++ rdbms/relationalDataBaseManagement/settings.py
@@ -129,8 +129,6 @@ STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 )
-
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 

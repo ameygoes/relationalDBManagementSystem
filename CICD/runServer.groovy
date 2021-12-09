9a1a99bd365e69d99ff07240165078c595176859
Amey <bamey2241997@gmail.com>
2021-12-09 23:20:52 +0530

    added changes

--- CICD/runServer.groovy
+++ CICD/runServer.groovy
@@ -13,10 +13,11 @@ pipeline{
 
     stage('Run Server'){
       steps{
-        sh "cd ${env.WORKSPACE}/CICD;pwd"
+        println("cd ${env.WORKSPACE}/CICD/")
+        sh "cd ${env.WORKSPACE}/CICD/;pwd"
         println("Inside WORKSPACE: ${env.WORKSPACE}/CICD")
-        sh "ls -lrt;sudo chmod 777 runServer.sh"
-        sh "sh runServer.sh ${env.WORKSPACE}"
+        sh "ls -lrt;sudo chmod 777 ${env.WORKSPACE}/CICD/runServer.sh"
+        sh "sh ${env.WORKSPACE}/CICD/runServer.sh ${env.WORKSPACE}"
       }
     }
   }
--- CICD/runServer.sh
+++ CICD/runServer.sh
@@ -4,9 +4,8 @@ sudo rm -rf $DESTINATION
 sudo mkdir -p $DESTINATION
 
 sudo cp -rvf $WORKSPACE/* $DESTINATION
-cd /usr/lib/python3.7/
-sudo chown -R $USER:$USER site-packages
-echo "/var/opt/rdbms/relationalDBManagementSystem" > addProjectPath.pth
+sudo chown -R $USER:$USER /usr/lib/python3.7/site-packages
+echo "/var/opt/rdbms/relationalDBManagementSystem" > /usr/lib/python3.7/addProjectPath.pth
 
 cd $DESTINATION/rdbms/
 echo "Starting Server..."

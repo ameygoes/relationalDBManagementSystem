e77c34821f11f60a2ae6a46767b73ae7e7f9ca74
Amey <bamey2241997@gmail.com>
2021-12-09 22:49:19 +0530

    added cahnges

--- CICD/runServer.sh
+++ CICD/runServer.sh
@@ -2,7 +2,7 @@ WORKSPACE=$1
 sudo rm -rf /var/opt/rdbms/relationalDBManagementSystem
 sudo mkdir -p /var/opt/rdbms/relationalDBManagementSystem
 
-sudo cp $WORKSPACE/**/*.* /var/opt/rdbms/relationalDBManagementSystem/
+sudo cp $WORKSPACE/CICD/**/*.* /var/opt/rdbms/relationalDBManagementSystem/
 cd /usr/lib/python3.7/
 sudo chown -R $USER:$USER site-packages
 echo "/var/opt/rdbms/relationalDBManagementSystem" > addProjectPath.pth

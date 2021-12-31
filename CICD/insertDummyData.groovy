a6c9c29f58d8ca397a9a39fd0bfc351f1f32ab27
ameygoes <bamey2241997@gmail.com>
2021-12-31 14:35:28 +0530

    added changes

--- CICD/insertDummyData.groovy
+++ CICD/insertDummyData.groovy
@@ -16,7 +16,7 @@ pipeline{
         sh "cd /var/lib/jenkins/workspace/runServer/backEnd/DummyDataGenerate;ls -lrt"
         println("Inside Folder: /var/lib/jenkins/workspace/runServer/backEnd/DummyDataGenerate")
         sh "ls -lrt;sudo chmod 777 /var/lib/jenkins/workspace/runServer/backEnd/DummyDataGenerate/DummyDataGeneration.py"
-        sh "python3 DummyDataGeneration.py"
+        sh "python3 /var/lib/jenkins/workspace/runServer/backEnd/DummyDataGenerate/DummyDataGeneration.py"
       }
     }
   }
